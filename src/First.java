6d4c8c17520ed98973b36c37f83904ca78ed3571
Renata <renatavel4@gmail.com>
2023-10-12 20:18:16 +0200

    Making changes

--- src/First.java
+++ src/First.java
@@ -1,8 +1,8 @@
 public class First {
     public static void main(String[] args) {
         System.out.println("Hello Vistula!");
-        System.out.println("Hi");
         System.out.println("hi there:)");
+        System.out.println("Hi!!");
     }
 
 }
